47f71f81a086b0789ac5d80ece15798378e45503
Aidan Harris <mail@aidanharris.io>
2017-02-12 22:38:57 +0000

    Fix cli-wrapper.sh

--- bin/cli-wrapper.sh
+++ bin/cli-wrapper.sh
@@ -11,8 +11,11 @@ if [ "$(which node 2>/dev/null)" != "" ] && [ -z "$NODE" ]
 then
   NODE="node"
 else
-  echo "Nodejs is not installed, please install it!"
-  exit 1
+  if [ -z "$NODE" ]
+  then
+    echo "Nodejs is not installed, please install it!"
+    exit 1
+  fi
 fi
 
 env "$NODE" "$CLI" "$@" | sed '1 s/^.*$/<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:media="http:\/\/search.yahoo.com\/mrss\/">/'
